397926673b76487c651835901328cf65e07fe157
Jkker <jerryjia@nyu.edu>
2021-05-11 04:54:56 +0800

    move analytics to doc & add font preconnect

--- pages/_app.js
+++ pages/_app.js
@@ -6,17 +6,6 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta content='width=device-width, initial-scale=1' name='viewport' />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `var _hmt = _hmt || [];
-					(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?64f5d58408d0054999987955b5f13fb4";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();`
-          }}
-        />
       </Head>
       <Component {...pageProps} />
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -4,18 +4,34 @@ import React from 'react'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang='en'>
         <Head>
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="theme-color" content="#ffffff" />
+          <meta name='msapplication-TileColor' content='#ffffff' />
+          <meta name='theme-color' content='#ffffff' />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `var _hmt = _hmt || [];
+					(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?64f5d58408d0054999987955b5f13fb4";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();`
+            }}
+          />
           <link
-            href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700;800&display=swap"
-            rel="stylesheet"
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='anonymous'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700;800&display=swap'
+            rel='stylesheet'
           />
           <noscript>
             <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Lexend&display=swap"
+              rel='stylesheet'
+              href='https://fonts.googleapis.com/css2?family=Lexend&display=swap'
             />
           </noscript>
         </Head>
